4dbf6ad74962b4b3d18970492cd29c2c33f8cae5
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-03-04 21:20:53 +0300

    init

--- network/01.sh
+++ network/01.sh
@@ -1,1 +1,2 @@
 ifconfig | expand | cut -c1-8 | sort | uniq -u | awk -F: '{print $1;}'
+# ifconfig -l
--- network/02.sh
+++ network/02.sh
@@ -1,1 +1,2 @@
 ifconfig en0 | grep inet | grep -v inet6 | awk -F" " '{print $6}'
+arp -a | awk -F" " '{print $2}'
